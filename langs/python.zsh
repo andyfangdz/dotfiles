3f90b4e67077db9360cae0686afcd8599dbe2a31
Andy Fang <andyfang.dz@gmail.com>
2018-02-27 23:32:50 -0500

    fix for pyenv

--- langs/python.zsh
+++ langs/python.zsh
@@ -1,4 +1,4 @@
-export PATH="/home/andy/.pyenv/bin:$PATH"
+path+=$HOME/.pyenv/bin
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 alias ipy='jupyter qtconsole --ConsoleWidget.font_family="Consolas" --ConsoleWidget.font_size=13'
