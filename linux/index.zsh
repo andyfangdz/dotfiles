e1c91131fe2e97a09327585c75e31a0eb17f140f
Andy Fang <andyfang.dz@gmail.com>
2018-03-07 00:36:08 -0500

    add terminal coloring

--- linux/index.zsh
+++ linux/index.zsh
@@ -3,3 +3,12 @@ function () {
     
     source ${script_dir}/gpu.zsh
 }
+
+if [ "$COLORTERM" = "gnome-terminal" ] || [ "$COLORTERM" = "xfce4-terminal" ]
+then
+    export TERM=xterm-256color
+elif [ "$COLORTERM" = "rxvt-xpm" ]
+then
+    export TERM=rxvt-256color
+fi
+
