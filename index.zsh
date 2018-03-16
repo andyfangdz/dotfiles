d684f86729a43b9da1ffa9f281ca6a9d08f8367e
Andy Fang <andyfang.dz@gmail.com>
2018-03-16 16:09:56 -0400

    organize

--- index.zsh
+++ index.zsh
@@ -1,20 +1,21 @@
 local script_dir=$(dirname ${(%):-%x})
 
-export ROOT_DIR="$script_dir"
+export ROOT_DIR="$script_dir/zsh"
+export local_zsh_dir="$ROOT_DIR"
 
 if [ -e ${script_dir}/local.config.zsh ]; then
   source ${script_dir}/local.config.zsh
 fi
 
-source ${script_dir}/00-utils.zsh
-source ${script_dir}/01-header.zsh
-source ${script_dir}/10-tools.zsh
+source ${local_zsh_dir}/00-utils.zsh
+source ${local_zsh_dir}/01-header.zsh
+source ${local_zsh_dir}/10-tools.zsh
 
-source ${script_dir}/20-zsh.zsh
+source ${local_zsh_dir}/20-zsh.zsh
 
-source ${script_dir}/common/index.zsh
-source ${script_dir}/langs/index.zsh
+source ${local_zsh_dir}/common/index.zsh
+source ${local_zsh_dir}/langs/index.zsh
 
-source ${script_dir}/80-platforms.zsh
-source ${script_dir}/90-misc.zsh
-source ${script_dir}/99-footer.zsh
+source ${local_zsh_dir}/80-platforms.zsh
+source ${local_zsh_dir}/90-misc.zsh
+source ${local_zsh_dir}/99-footer.zsh
