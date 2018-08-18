6a54cdcf63b861c38dd1334023ee8a950955dc4e
Andy Fang <andy.fang@airbnb.com>
2018-08-18 11:52:13 -0700

    emacs mode

--- zsh/30-keys.zsh
+++ zsh/30-keys.zsh
@@ -1,4 +1,4 @@
-bindkey -v '^?' backward-delete-char
+# bindkey -v '^?' backward-delete-char
 zle     -N   fzf-history-widget
 bindkey '^R' fzf-history-widget
-
+set -o emacs
--- zsh/90-misc.zsh
+++ zsh/90-misc.zsh
@@ -3,23 +3,12 @@ if [ "$DISABLE_ITERM" != true ] ; then
     test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 fi
 
-function upload_gem {
-    username=`whoami`
-    # resolve the DNS entry only once,
-    # otherwise the commands might go to different hosts
-    host=$(dig +short geminabox-internal.aws.airbnb.com|head -n1|sed s'/\.$//')
-  
-    # Upload to gem server and add it
-    scp $1 ${username}@${host}:~ && \
-        ssh ${username}@${host} "add_gem.rb $1; rm $1"
-}
-
 export GPG_TTY=$(tty)
 [ -f $HOME/.travis/travis.sh ] && source $HOME/.travis/travis.sh
 
 export SCRATCH_PATH="$HOME/Dropbox/scratchpad.md"
 
-alias vis="vim $SCRATCH_PATH" 
+alias vis="vim $SCRATCH_PATH"
 alias fzs="cat $SCRATCH_PATH | fzf --tac --ansi | tee >(tr -d '\n\r' | pbcopy)"
 alias adds="cat >> $SCRATCH_PATH"
 alias codes="code $SCRATCH_PATH"
